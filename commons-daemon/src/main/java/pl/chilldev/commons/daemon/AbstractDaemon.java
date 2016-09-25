f14e07ee7044bc8b072d5fe836667ae74052b4b7
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2016-09-25 09:57:52 +0200

    Abstract daemon class that wraps common flow for entry points.

---
+++ commons-daemon/src/main/java/pl/chilldev/commons/daemon/AbstractDaemon.java
@@ -0,0 +1,74 @@
+/**
+ * This file is part of the ChillDev-Commons.
+ *
+ * @license http://mit-license.org/ The MIT license
+ * @copyright 2016 © by Rafał Wrzeszcz - Wrzasq.pl.
+ */
+
+package pl.chilldev.commons.daemon;
+
+import org.apache.commons.daemon.Daemon;
+import org.apache.commons.daemon.DaemonContext;
+import org.apache.commons.daemon.DaemonUserSignal;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Generic daemon application stub.
+ */
+public abstract class AbstractDaemon implements Daemon, DaemonUserSignal
+{
+    /**
+     * Logger.
+     */
+    protected Logger logger = LoggerFactory.getLogger(AbstractDaemon.class);
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void signal()
+    {
+        this.logger.info("Handling configuration reload.");
+
+        // no deep in-sight for now, but at least JVM bootstrapping is avoided
+
+        try {
+            this.stop();
+            this.start();
+            //CHECKSTYLE:OFF: IllegalCatchCheck
+        } catch (Exception error) {
+            //CHECKSTYLE:ON: IllegalCatchCheck
+            this.logger.error("Failed to handle signal call.", error);
+            throw new RuntimeException(error);
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void init(DaemonContext context)
+    {
+        // diagnostic info
+        this.logger.info(
+            "Started {} daemon v.{} on {} {} ({}) / Java {} ({}).",
+            this.getClass().getPackage().getName(),
+            Package.DEFAULT_PACKAGE.getVersion(),
+            System.getProperty("os.name"),
+            System.getProperty("os.version"),
+            System.getProperty("os.arch"),
+            System.getProperty("java.version"),
+            System.getProperty("java.vendor")
+        );
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void destroy()
+    {
+        // dummy method; not used - needed by org.apache.commons.daemon.Daemon
+    }
+}
---
+++ commons-daemon/src/test/java/test/pl/chilldev/commons/daemon/AbstractDaemonTest.java
@@ -0,0 +1,55 @@
+/**
+ * This file is part of the ChillDev-Commons.
+ *
+ * @license http://mit-license.org/ The MIT license
+ * @copyright 2016 © by Rafał Wrzeszcz - Wrzasq.pl.
+ */
+
+package test.pl.chilldev.commons.daemon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.mockito.Spy;
+import org.mockito.runners.MockitoJUnitRunner;
+import pl.chilldev.commons.daemon.AbstractDaemon;
+
+@RunWith(MockitoJUnitRunner.class)
+public class AbstractDaemonTest
+{
+    @Spy
+    private AbstractDaemon daemon = new AbstractDaemon()
+    {
+        @Override
+        public void start() throws Exception
+        {
+            // dummy method
+        }
+
+        @Override
+        public void stop() throws Exception
+        {
+            // dummy method
+        }
+    };
+
+    @Test
+    public void signal() throws Exception
+    {
+        // just for code coverate
+        this.daemon.init(null);
+        this.daemon.destroy();
+
+        this.daemon.signal();
+
+        Mockito.verify(this.daemon).start();
+        Mockito.verify(this.daemon).stop();
+    }
+
+    @Test(expected = RuntimeException.class)
+    public void signalThrowsException() throws Exception
+    {
+        Mockito.doThrow(Exception.class).when(this.daemon).start();
+        this.daemon.signal();
+    }
+}
