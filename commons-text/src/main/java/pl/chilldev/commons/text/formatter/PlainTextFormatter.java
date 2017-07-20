b1c1dd3d72dd0b687c3972b461ccd9d1d231a284
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2017-07-20 23:24:30 +0200

    Migrated from Apache Commons Lang3 to Apache Commons Text.

--- commons-text/src/main/java/pl/chilldev/commons/text/formatter/PlainTextFormatter.java
+++ commons-text/src/main/java/pl/chilldev/commons/text/formatter/PlainTextFormatter.java
@@ -2,12 +2,12 @@
  * This file is part of the ChillDev-Commons.
  *
  * @license http://mit-license.org/ The MIT license
- * @copyright 2016 © by Rafał Wrzeszcz - Wrzasq.pl.
+ * @copyright 2016 - 2017 © by Rafał Wrzeszcz - Wrzasq.pl.
  */
 
 package pl.chilldev.commons.text.formatter;
 
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.apache.commons.text.StringEscapeUtils;
 
 /**
  * Plain text format handler.
