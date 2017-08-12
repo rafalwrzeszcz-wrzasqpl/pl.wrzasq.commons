28cc6e8ebec90be10ef7a6473c96885c2f8958c8
Rafał Wrzeszcz <rafal.wrzeszcz@wrzasq.pl>
2017-08-12 03:05:50 +0200

    Continous deployment for releases updates through Travis CI.

---
+++ .travis/release.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+##
+# This file is part of the ChillDev-Commons.
+#
+# @license http://mit-license.org/ The MIT license
+# @copyright 2017 © by Rafal Wrzeszcz - Wrzasq.pl.
+##
+
+set -ex
+
+REPO=`git config remote.origin.url`
+SSH_REPO=${REPO/https:\/\/github.com\//git@github.com:}
+
+# prevent looping
+if [ "${TRAVIS_COMMIT_MESSAGE:0:6}" == "[auto]" ] ; then
+    exit
+fi
+
+# change origin URL to SSH to allow uploading with SSH key
+git remote rm origin
+git remote add origin ${SSH_REPO}
+
+# make sure HEAD points to the branch
+git checkout ${TRAVIS_BRANCH}
+
+# first make current version release
+mvn versions:set versions:commit -DremoveSnapshot=true
+git add -u
+git commit -m "[auto] Automated release release."
+git push origin ${TRAVIS_BRANCH}:master
+
+# now create a new version commit
+mvn build-helper:parse-version versions:set versions:commit \
+    -DnewVersion="\${semver.majorVersion}.\${semver.minorVersion}.\${semver.nextIncrementalVersion}-SNAPSHOT"
+git add -u
+git commit -m "[skip ci] New version bump."
+git push origin ${TRAVIS_BRANCH}
