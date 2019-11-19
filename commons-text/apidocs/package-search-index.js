5179a176945d045dbd08f10420185899bae769ca
Travis CI User <travis@example.org>
2019-11-19 22:07:01 +0000

    wagon-git commit

---
+++ commons-text/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"pl.wrzasq.commons.text"},{"l":"pl.wrzasq.commons.text.formatter"},{"l":"pl.wrzasq.commons.text.html"},{"l":"pl.wrzasq.commons.text.slugifier"}]
