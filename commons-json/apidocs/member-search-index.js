2e54a1f1d40f8496f76e01b34efef306705b8d5a
Travis CI User <travis@example.org>
2020-06-13 19:46:56 +0000

    wagon-git commit

---
+++ commons-json/apidocs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"pl.wrzasq.commons.json","c":"ObjectMapperFactory","l":"createObjectMapper()"},{"p":"pl.wrzasq.commons.json","c":"ObjectMapperFactory","l":"ObjectMapperFactory()","url":"%3Cinit%3E()"}]
